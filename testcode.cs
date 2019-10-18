1478ff516b08fc74bc61fb3e0e4064de343b6f0f
azuresamples <54381444+azuresamples@users.noreply.github.com>
2019-10-18 16:32:02 +0900

    Create testcode.cs

---
+++ testcode.cs
@@ -0,0 +1,119 @@
+using System;
+    using System.Collections.Specialized;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Web;
+
+    namespace ClientCertificateUsageSample
+    {
+        public partial class cert : System.Web.UI.Page
+        {
+            public string certHeader = "";
+            public string errorString = "";
+            private X509Certificate2 certificate = null;
+            public string certThumbprint = "";
+            public string certSubject = "";
+            public string certIssuer = "";
+            public string certSignatureAlg = "";
+            public string certIssueDate = "";
+            public string certExpiryDate = "";
+            public bool isValidCert = false;
+
+            //
+            // Read the certificate from the header into an X509Certificate2 object
+            // Display properties of the certificate on the page
+            //
+            protected void Page_Load(object sender, EventArgs e)
+            {
+                NameValueCollection headers = base.Request.Headers;
+                certHeader = headers["X-ARR-ClientCert"];
+                if (!String.IsNullOrEmpty(certHeader))
+                {
+                    try
+                    {
+                        byte[] clientCertBytes = Convert.FromBase64String(certHeader);
+                        certificate = new X509Certificate2(clientCertBytes);
+                        certSubject = certificate.Subject;
+                        certIssuer = certificate.Issuer;
+                        certThumbprint = certificate.Thumbprint;
+                        certSignatureAlg = certificate.SignatureAlgorithm.FriendlyName;
+                        certIssueDate = certificate.NotBefore.ToShortDateString() + " " + certificate.NotBefore.ToShortTimeString();
+                        certExpiryDate = certificate.NotAfter.ToShortDateString() + " " + certificate.NotAfter.ToShortTimeString();
+                    }
+                    catch (Exception ex)
+                    {
+                        errorString = ex.ToString();
+                    }
+                    finally 
+                    {
+                        isValidCert = IsValidClientCertificate();
+                        if (!isValidCert) Response.StatusCode = 403;
+                        else Response.StatusCode = 200;
+                    }
+                }
+                else
+                {
+                    certHeader = "";
+                }
+            }
+
+            //
+            // This is a SAMPLE verification routine. Depending on your application logic and security requirements, 
+            // you should modify this method
+            //
+            private bool IsValidClientCertificate()
+            {
+                // In this example we will only accept the certificate as a valid certificate if all the conditions below are met:
+                // 1. The certificate is not expired and is active for the current time on server.
+                // 2. The subject name of the certificate has the common name nildevecc
+                // 3. The issuer name of the certificate has the common name nildevecc and organization name Microsoft Corp
+                // 4. The thumbprint of the certificate is 30757A2E831977D8BD9C8496E4C99AB26CB9622B
+                //
+                // This example does NOT test that this certificate is chained to a Trusted Root Authority (or revoked) on the server 
+                // and it allows for self signed certificates
+                //
+
+                if (certificate == null || !String.IsNullOrEmpty(errorString)) return false;
+
+                // 1. Check time validity of certificate
+                if (DateTime.Compare(DateTime.Now, certificate.NotBefore) < 0 || DateTime.Compare(DateTime.Now, certificate.NotAfter) > 0) return false;
+
+                // 2. Check subject name of certificate
+                bool foundSubject = false;
+                string[] certSubjectData = certificate.Subject.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in certSubjectData)
+                {
+                    if (String.Compare(s.Trim(), "CN=nildevecc") == 0)
+                    {
+                        foundSubject = true;
+                        break;
+                    }
+                }
+                if (!foundSubject) return false;
+
+                // 3. Check issuer name of certificate
+                bool foundIssuerCN = false, foundIssuerO = false;
+                string[] certIssuerData = certificate.Issuer.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in certIssuerData)
+                {
+                    if (String.Compare(s.Trim(), "CN=nildevecc") == 0)
+                    {
+                        foundIssuerCN = true;
+                        if (foundIssuerO) break;
+                    }
+
+                    if (String.Compare(s.Trim(), "O=Microsoft Corp") == 0)
+                    {
+                        foundIssuerO = true;
+                        if (foundIssuerCN) break;
+                    }
+                }
+
+                if (!foundIssuerCN || !foundIssuerO) return false;
+
+                // 4. Check thumprint of certificate
+                if (String.Compare(certificate.Thumbprint.Trim().ToUpper(), "30757A2E831977D8BD9C8496E4C99AB26CB9622B") != 0) return false;
+
+                return true;
+            }
+        }
+    }
